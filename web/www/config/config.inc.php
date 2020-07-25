e4c99da0c99317ee70bae6938d8fa6add249f7a4
Felipe Barros <felipetech@tuta.io>
2020-07-25 16:24:32 -0300

    chore: Add and configure docker

---
+++ web/www/config/config.inc.php
@@ -0,0 +1,10 @@
+<?php
+session_start();
+
+define('BASE_URL', 'http://localhost:8080/');
+
+define('DB_DRIVER', 'mysql');
+define('DB_HOST', '172.10.0.2');
+define('DB_NAME', 'php_db');
+define('DB_USER', 'root');
+define('DB_PASSWORD', 'toor');
